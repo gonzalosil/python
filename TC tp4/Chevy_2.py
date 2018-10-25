b3148c2b002e3d656311daaf6191b109df04e1a4
Gonzalo Silva <gsilva@itba.edu.ar>
2018-10-25 13:01:05 -0300

    chevy 2

--- TC tp4/Chevy_2.py
+++ TC tp4/Chevy_2.py
@@ -14,7 +14,7 @@ import General_aprox as denorm
 class Chevy_2(object):
     def __init__(self, Ap, As, Wp, Ws, type, Wp_mas=None, Ws_mas=None):
                 #si se quiere hacer un band pass o un band reject, Wp y Ws se usan como Wp- y Ws-
-        e=1/(m.sqrt(m.pow(10,As/10)-1))
+        e=1/m.sqrt(m.pow(10,As/10)-1)
 
         if (type == "LP"):
             WsN = Ws/Wp
@@ -26,26 +26,40 @@ class Chevy_2(object):
             WsN = (Wp_mas-Wp)/(Ws_mas-Ws)
 
         n=m.ceil(m.acosh((m.sqrt(m.pow(10,As/10)-1))/(m.sqrt(m.pow(10,Ap/10)-1)))/(m.acosh(WsN)))
-
+       # n=m.ceil(m.acosh((m.sqrt(m.pow(10,As/10)-1))/e)/(m.acosh(WsN)))
         #hallo los polos de chevy
+        print("hola mijo")
         alpha = []
+        beta = []
+        sigma = []
+        omega = []
         poles = []
         zeros = []
       
-        for k in range (0,2*n):
+        for k in range (0,n):
             alpha.append([(2*(k+1)-1)/(2*n)*m.pi])
 
-        beta = 1/n*m.asinh(1/e)
+        beta.append([1/n*m.asinh(1/e)])
+        beta.append([-1/n*m.asinh(1/e)])
 
-        for k in range (0,2*n): #hallo los polos
-            if (1/(complex(m.sin(alpha[k][0])*m.sinh(beta), m.cos(alpha[k][0]*m.cosh(beta))))).real < 0:
-                poles.append(WsN*(1/(complex(m.sin(alpha[k][0])*m.sinh(beta), m.cos(alpha[k][0]*m.cosh(beta))))))
+       # print(beta[0][0])
+        for k in range (0,n):
+            if m.sin(alpha[k][0])*m.sinh(beta[0][0]) < 0: #tomo solo los polos con parte real negativa
+                sigma.append(m.sin(alpha[k][0])*m.sinh(beta[0][0]))
+                omega.append(m.cos(alpha[k][0])*m.cosh(beta[0][0])) #para omega es lo mismo si uso beta(0) o beta(1) ya que el cos es par
+            else:
+                sigma.append(m.sin(alpha[k][0])*m.sinh(beta[1][0]))
+                omega.append(m.cos(alpha[k][0])*m.cosh(beta[0][0]))
+
+        for k in range (0,len(sigma)):
+            poles.append(1/complex(sigma[k],omega[k]))
 
         #print (poles)
         for k in range (0,n):
-            zeros.append(WsN*complex(0,1/m.cos(alpha[k][0])))
+            zeros.append(complex(0,1/m.cos(alpha[k][0])))
         #print (zeros)
         print(n) 
+
         self.num, self.den = signal.zpk2tf(zeros,poles,1)
        #print(self.den)
         k = self.den[len(self.den)-1]/self.num[len(self .num)-1]
@@ -54,6 +68,7 @@ class Chevy_2(object):
        # pyplot.semilogx(self.w,-self.mag)
         #pyplot.show()
         self.H = denorm.General_aprox.denormalization(self,type,Wp,n,poles,zeros)
+        
 
 
     def get_transfer(self):
@@ -61,7 +76,7 @@ class Chevy_2(object):
 
 
 if __name__ == "__main__":
-    ex1= Chevy_2(3,50,1256.64,5026.55, "LP")
+    ex1= Chevy_2(10,100,1000,2000, "LP")
     ex = Chevy_2.get_transfer(ex1)
     w,mag,phase = signal.bode(ex, None, 10000)
     pyplot.semilogx(w,-mag)
