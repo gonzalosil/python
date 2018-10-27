41ae0cbdf0674074e9ab168a49321e956fefd493
amartorell <amartorell@itba.edu.ar>
2018-10-26 22:29:43 -0300

    asdasd

--- TC tp4/newstaffpy.py
+++ TC tp4/newstaffpy.py
@@ -19,3 +19,21 @@
     #     self.axis3.clear()
     #     w,mag,phase=signal.bode(tf)
     #     self.plotMag_Etapa2(w,mag)
+
+      #def Se_Apreto_Graph_Etapas_Polos(self):
+
+      # aux=[]
+      # arreglo_TF=[]
+      # for i in range (0,self.List_Etapas_Polos.size()):
+      #     extra=complex(self.List_Etapas_Polos.get(i))
+      #     control=np.conjugate(extra)
+      #     if( cuentas.comparar(extra,control)):
+      #        aux.append(extra)
+      #        self.TransferFun_Etapa2=scipy.signal.ZerosPolesGain([],[(np.abs(np.real(extra)))],(np.abs(np.real(extra))))
+      #        arreglo_TF.append(self.TransferFun_Etapa2)
+
+      #     else:
+      #         aux.append(extra)
+      #         aux.append(np.conjugate(extra))
+      #         self.TransferFun_Etapa2=scipy.signal.ZerosPolesGain([],[np.conjugate(extra),extra],(np.abs(extra))**2)
+      #         arreglo_TF.append(self.TransferFun_Etapa2)
